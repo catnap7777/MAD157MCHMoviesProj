b7cad5f0372e8723d88360b0c099c8e096dd3c55
km <35552602+catnap7777@users.noreply.github.com>
2020-09-22 18:01:25 -0500

    more

---
+++ MAD157MCHMoviesProj/HomeVC.swift
@@ -0,0 +1,28 @@
+//
+//  HomeVC.swift
+//  MAD157MCHMoviesProj
+//
+//  Created by Karen Mathes on 9/22/20.
+//  Copyright © 2020 TygerMatrix Software. All rights reserved.
+//
+
+import UIKit
+
+class HomeVC: UIViewController {
+
+    @IBOutlet var enterButtonObj: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func enterButtonPressed(_ sender: Any) {
+        
+        
+    }
+    
+
+}
